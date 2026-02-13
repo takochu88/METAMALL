89ab4951d156a3fde01f3cd7dcd29f70ccc9db1d
takochu88 <168629454+takochu88@users.noreply.github.com>
2026-02-14 01:26:00 +0900

    [feat] Main作成

---
+++ Assets/Core/Main.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("[Main] Awake");
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
